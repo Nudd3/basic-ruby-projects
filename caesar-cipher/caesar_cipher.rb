d80cd91863d9f3f92ea1e267143127bdceef4431
Noak Pettersson <noake12@gmail.com>
2021-07-18 13:51:31 +0200

    caesar-cipher done working

---
+++ caesar-cipher/caesar_cipher.rb
@@ -0,0 +1,19 @@
+require 'pry-byebug'
+def transform(string, key)
+  string.chars.map do |char|
+
+    char = char.ord
+    
+    ascii_start = char < 97 ? 65 : 97
+    
+    if char.between?(65, 90) || char.between?(97, 122)
+      char = (((char - ascii_start) + key) % 26) + ascii_start
+    end
+    char.chr
+  end.join
+  
+end
+
+puts transform("CAT!", 5)
+puts transform("What a string!", 5)
+puts transform("computer", 7)
