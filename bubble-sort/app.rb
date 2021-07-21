9c60ece25b7c6b71dc397c0754410e84ad46e125
Noak Pettersson <noake12@gmail.com>
2021-07-21 17:24:38 +0200

    corrected formatting

--- bubble-sort/app.rb
+++ bubble-sort/app.rb
@@ -1,11 +1,9 @@
 def bubble_sort(array)
   n = array.length
-  while true 
-
+  while true
     swapped = false
 
     (n - 1).times do |i|
-
       if (array[i] > array[i + 1])
         array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
@@ -16,17 +14,14 @@ def bubble_sort(array)
   array
 end
 
-
-
 def bubble_sort_with_comments(array)
   n = array.length
-  
+
   while true
     p array
     swapped = false
 
     (n - 1).times do |i|
-      
       if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
         p "#{array[i + 1]} and #{array[i]} swapped places"
@@ -39,5 +34,5 @@ def bubble_sort_with_comments(array)
 
   array
 end
-p bubble_sort([4,3,78,2,0,2])
-p bubble_sort_with_comments([5,4,3,2,1,0])
+p bubble_sort([4, 3, 78, 2, 0, 2])
+p bubble_sort_with_comments([5, 4, 3, 2, 1, 0])
