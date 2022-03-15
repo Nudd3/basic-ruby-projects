75f08c2698121e89d8c4ed7653b7f7856faff64d
Noak Pettersson <noake12@gmail.com>
2022-03-15 13:12:59 +0100

    Formatting

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,21 +1,16 @@
-require 'pry-byebug'
 def transform(string, key)
   string.chars.map do |char|
-
     char = char.ord
-    
+
     ascii_start = char < 97 ? 65 : 97
-    
-    if char.between?(65, 90) || char.between?(97, 122)
-      char = (((char - ascii_start) + key) % 26) + ascii_start
-    end
+
+    char = (((char - ascii_start) + key) % 26) + ascii_start if char.between?(65, 90) || char.between?(97, 122)
     char.chr
   end.join
-  
 end
 
-puts transform("CAT!", 5)
-puts transform("What a string!", 5)
-puts transform("computer", 7)
-puts transform("z", 8)
-puts transform("Z", 9)
+puts transform('CAT!', 5)
+puts transform('What a string!', 5)
+puts transform('computer', 7)
+puts transform('z', 8)
+puts transform('Z', 9)
